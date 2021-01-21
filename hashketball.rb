c2a36aef73b1374658e574364cfd7b46028cc296
Gus Bikos <gusbikos@gmail.com>
2021-01-21 20:40:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -51,7 +51,7 @@ def game_hash
           {:assists => "2"},
           {:steals => "4"},
           {:blocks => "11"},
-          {:slam_dunks => "1"},
+          {:slam_dunks => "1"}
     ],
     :away => {
       :team_name => " ",
