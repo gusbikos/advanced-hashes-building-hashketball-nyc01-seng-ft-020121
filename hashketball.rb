c6d9148aef82c6ad547df6c2ff8ea7f196567338
Gus Bikos <gusbikos@gmail.com>
2021-01-21 20:59:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,7 +7,7 @@ def game_hash
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
       :players => [
-        {:player_name: "Alan Anderson",
+        {player_name: "Alan Anderson",
           number: "0",
           points: "12",
           assists: "12",
