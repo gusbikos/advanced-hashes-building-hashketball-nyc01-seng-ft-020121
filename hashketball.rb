47c8e48fdac067c506fd02a07b1bfea8bc1737ec
Gus Bikos <gusbikos@gmail.com>
2021-01-21 18:37:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,26 @@
 # Write your code here!
+require 'pry'
+def game_hash
+
+  stats = {
+    :home => {
+      :team_name => " ",
+      :colors => [" "],
+      :players => [
+        {:player_name => "Alan Anderson"},
+
+
+
+
+
+  ],
+    :away => {
+      :team_name => " ",
+      :colors => [" "],
+      :players => [{}]
+    }
+  }
+
+
+}
+end
