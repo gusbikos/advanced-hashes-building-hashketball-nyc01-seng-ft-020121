617c52e676868ab1506544debfe299e33a166ccc
Gus Bikos <gusbikos@gmail.com>
2021-01-21 21:16:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -54,9 +54,21 @@ def game_hash
           :slam_dunks => 1}
     ] },
     :away => {
-      :team_name => " ",
-      :colors => [" "],
-      :players => [{}]
+      :team_name => "Charlotte Hornets",
+      :colors => ["Turquoise", "Purple"],
+      :players => [
+        {player_name: "Jeff Adrien",
+          :number => 4,
+          :shoe => 18,
+          :points => 10,
+          :rebounds => 1,
+          :assists => 1,
+          :steals => 2,
+          :blocks => 7,
+          :slam_dunks => 2},
+
+          
+      ]
     }
 
 }
