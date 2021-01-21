94bc99fa26cdcd8d29743afe4ab21dbf76d510c4
Gus Bikos <gusbikos@gmail.com>
2021-01-21 18:42:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -10,7 +10,11 @@ def game_hash
         {:player_name => "Alan Anderson"},
         {:number => "0"},
         {:shoe => "16"},
-
+        {:points => "22"},
+        {:rebounds => "12"},
+        {:assists => "12"},
+        {:steals => "3"},
+        {:blocks => "1"},
 
 
 
